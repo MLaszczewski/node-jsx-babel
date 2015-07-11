0be0fb1cb982e278f5481caa6fb7356a2c5fbb40
Meettya <meettya@gmail.com>
2015-07-12 00:17:32 +0300

    * change to babel
    + add try-catch block to additional transform

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var fs = require('fs');
-var jstransform = require('jstransform/simple');
+var fs    = require('fs');
+var babel = require('babel-core');
 
 var installed = false;
 
@@ -10,21 +10,20 @@ function install(options) {
 
   options = options || {};
 
-  // Import everything in the transformer codepath before we add the import hook
-  jstransform.transform('', options);
-
   require.extensions[options.extension || '.js'] = function(module, filename) {
-    if (!options.hasOwnProperty("react"))
-      options.react = true;
 
     var src = fs.readFileSync(filename, {encoding: 'utf8'});
     if (typeof options.additionalTransform == 'function') {
-      src = options.additionalTransform(src);
+      try {
+        src = options.additionalTransform(src, filename);
+      } catch (e) {
+        throw Error('Error additional transforming ' + filename + ' ' + e.toString());
+      }
     }
     try {
-      src = jstransform.transform(src, options).code;
+      src = babel.transform(src, { filename: filename, ast : false }).code;
     } catch (e) {
-      throw new Error('Error transforming ' + filename + ' to JS: ' + e.toString());
+      throw Error('Error transforming ' + filename + ' to JS: ' + e.toString());
     }
     module._compile(src, filename);
   };
